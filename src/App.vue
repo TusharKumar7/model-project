e5d061e8d4bba2777fc82e8ce95b8cbec2dc0490
TusharKumar7 <tusharkumar@innotechteam.in>
2024-04-22 14:37:11 +0530

    backdrop added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <modal-component></modal-component>
+  <div class="app-container">
+    <h1>My First Vue App</h1>
+    <div class="input-container">
+      <input type="text" ref="focusInput" />
+    </div>
+    <div class="btn-container">
+      <button id="focus-btn" @click="focusInput()">Focus</button>
+      <button id="modal-btn" @click="showModal = true">Open Modal</button>
+    </div>
+  </div>
+  <div class="modal" v-if="showModal" @click="closeModal()">
+    <ModalComponent
+      title="Sign up for the Giveaway!"
+      content="Grab Your ninja swag for half price!"
+    />
+  </div>
 </template>
 
 <script>
-import ModalComponent from './components/ModalComponent.vue'
-
+import ModalComponent from "./components/ModalComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     ModalComponent,
-  }
-}
+  },
+  data: function () {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    focusInput() {
+      setTimeout(() => {
+        this.$refs.focusInput.focus();
+      }, 1);
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  position: relative;
+}
+
+.btn-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.input-container input {
+  border: 1px solid #64748b;
+  border-radius: 3px;
+  font-size: 1rem;
+  padding: 3px 6px;
+}
+
+.btn-container button {
+  color: white;
+  background-color: #2563eb;
+  outline: none;
+  border: none;
+  padding: 7px 12px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+}
+
+.btn-container button:hover {
+  color: #2563eb;
+  background-color: #fff;
+  border: 1px solid #2563eb;
 }
 </style>
--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,20 +1,64 @@
 <template>
-    <h1>My First Vue App</h1>
+<div class="main-container">
+  <div class="modal-container">
+    <div class="modal-title">{{ title }}</div>
+    <div class="modal-content">{{ content }}</div>
+  </div>
+</div>
+
 </template>
 
 <script>
 export default {
-    name:"ModalComponent",
-    data(){
-        return{
-            
-        }
-    }
-}
+  name: "ModalComponent",
+  data() {
+    return {};
+  },
+  props: {
+    title: String,
+    content: String,
+  },
+};
 </script>
 
 <style scoped>
-    h1{
-        margin-top: 2rem;
-    }
+.main-container {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  cursor: pointer;
+  position: absolute;
+  z-index: 1;
+}
+
+.modal-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px;
+  background: #fff;
+  border-radius: 10px;
+  gap: 30px;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+  width: 25%;
+  margin: 0 auto;
+  position: absolute;
+  inset: 0;
+  top: 40%;
+  height: max-content;
+  z-index: 2;
+  cursor: default;
+}
+
+.modal-title {
+  color: #05b7b0;
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+
+.modal-content {
+  color: gray;
+}
 </style>
